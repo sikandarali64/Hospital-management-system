29414412fef97278dbf07585149a8806eecfa449
Sikandar ali buriro <sikandarali64@users.noreply.github.com>
2026-03-07 08:05:49 +0500

    Initialize project with 5 core files

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Hospital Management System App Initialized');
+    // Add logic for dynamic navigation, alerts, forms, etc.
+});
